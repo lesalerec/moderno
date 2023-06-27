4d5c13a7c674f9d3c9d6662a8e2bcb244db737f9
Yavorsky <lesalerec@gmail.com>
2023-06-27 23:36:29 +0300

    added product slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function () {
-  
+  $('.star').rateYo({
+    starWidth: '12px',
+    normalFill: '#707070',
+    ratedFill: '#ffa726',
+    readOnly: true,
+  });
+
+  var swiper = new Swiper('.product__slider-inner', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        allowTouchMove: true,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        allowTouchMove: true,
+        slidesPerGroup: 3,
+      },
+    },
+  });
+
+  var mixer = mixitup('.products__content');
 });
